8b5f8b63a486a97dabd23b4df20fb9d8cac7f244
Kazure Zheng <kazure@debian>
2026-03-20 21:14:04 +0800

    some modify

--- uva-tools.el
+++ uva-tools.el
@@ -45,4 +45,3 @@ Codes: 1 ANSI, 2 JAVA, 3 C++, 4 Pascal, 5 C++11, 6 Python."
   (let ((id (file-name-base))
         (file-name (buffer-file-name)))
     (uva-tool--submit id file-name uva-tool-language)))
-
