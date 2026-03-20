c7b0aeeddf6e4bcd9294ef64dc9e43f3c9a1da4f
Kazure Zheng <kazure@debian>
2026-03-20 20:48:30 +0800

    some modify

--- uva-tools.el
+++ uva-tools.el
@@ -1,3 +1,12 @@
+(defgroup uva-tool nil
+  "uva-tool client"
+  :prefix 'uva-tool-
+  :group 'tools)
+
+(defcustom uva-tol-username 'default
+  "Your username in UVA"
+  :type 'string)
+
 (defun uva-tool--hunt (username num)
   "Fetch recent summit runs for username with uva-tool."
   (async-shell-command (format "uva-tool -hunt -u %s -r %s" username num)))
