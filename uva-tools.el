8d8d688c2b5736dcb6249292ce004e0610563056
Kazure Zheng <kazure@debian>
2026-03-20 20:53:18 +0800

    support to hunt my own submits

--- uva-tools.el
+++ uva-tools.el
@@ -15,3 +15,10 @@
   "Submit a problem to UVA with uva-tool"
   (async-shell-command (format "uva-tool -submit %s %s %s"
                                id file-name lang)))
+
+;;;###autoload
+(defun uva-tool-hunt-myself ()
+  "Fetch recent submits for yourself with uva-tool"
+  (interactive)
+  (uva-tool--hunt uva-tool-username 10))
+
